dc4ddaa488ee59d9b9251ab9f70e06174fdcd656
TiffanyStPierre <stpierre.tiffany@gmail.com>
2023-11-07 08:41:48 -0700

    move character counter function to separate .js file

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#tweet-text").on("input", function() {
+    let remainingCharacters = 140 - $(this).val().length;
+    let $counter = $(this).siblings().children(".counter");
+    $counter.val(remainingCharacters);
+
+    //If the remaining character count is less than zero, change the font to red
+    if (remainingCharacters < 0) {
+      $counter.addClass('negative-color');
+    } else {
+      $counter.removeClass('negative-color');
+    }
+  });
+});

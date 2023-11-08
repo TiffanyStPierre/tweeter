997a54218ab7c1d961781786946e177ad771a76b
TiffanyStPierre <stpierre.tiffany@gmail.com>
2023-11-08 06:06:11 -0700

    create function to get tweets using ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,31 +5,6 @@
  */
 
 
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 $(document).ready(function() {
 
   const createTweetElement = function(tweetObject) {
@@ -62,13 +37,19 @@ $(document).ready(function() {
     });
   };
 
-  renderTweets(tweetData);
-
   $("#tweet-form").on("submit", function(event) {
     event.preventDefault();
     $.post("/tweets", $(this).serialize(), function(data) {
       $(".result").html(data);
     })
   })
+
+  const loadTweets = function() {
+    $.get("/tweets", function(data) {
+      renderTweets(data);
+    })
+  };
+
+  loadTweets();
 }
 );
